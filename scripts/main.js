ebd962d3408751760f82dd144b995ba2c98b9c93
Kevin Vilyan <146146416+Kev-Vily@users.noreply.github.com>
2025-02-26 10:27:07 +0700

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const asthosus = Vars.mods.locateMod("asthosus").meta;
+
+asthosus.displayName = Core.bundle.get("mod.name");
+asthosus.author = Core.bundle.get("mod.author");
+asthosus.description = Core.bundle.get("mod.description");
+asthosus.subtitle = Core.bundle.get("mod.subtitle");
